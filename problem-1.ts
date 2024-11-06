debccdcd3d4b5e519f9f8048d5317ac2cd2457e5
unknown <atikurahad@gmail.com>
2024-11-06 19:15:56 +0600

    1st problem solved

---
+++ problem-1.ts
@@ -0,0 +1,12 @@
+/*
+Problem 1 
+*/
+
+function sumArray(arr: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+  console.log(sumArray([2,3,4,5,6]));
